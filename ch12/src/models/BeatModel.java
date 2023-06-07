5053dc35c211281ffff936f8c827c051d429274f
Graham Strickland <grahamhstrickland@gmail.com>
2023-06-07 21:30:18 +0200

    Add ch12

---
+++ ch12/src/models/BeatModel.java
@@ -0,0 +1,63 @@
+package models;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.Line;
+
+import models.interfaces.BeatModelInterface;
+
+public class BeatModel implements BeatModelInterface, Runnable {
+    List<BeatObserver> beatObservers = new ArrayList<BeatObserver>();
+    List<BPMObserver> bpmObservers = new ArrayList<BPMObserver>();
+    int bpm = 90;
+    Thread thread;
+    boolean stop = false;
+    Clip clip;
+
+    public void initialize() {
+        try {
+            File resource = new File("clap.wav");
+            clip = (Clip) AudioSystem.getLine(new Line.Info(Clip.class));
+            clip.open(AudioSystem.getAudioInputStream(resource));
+        }
+        catch (Exception ex) { /* ... */ }
+    }
+
+    public void on() {
+        bpm = 90;
+        notifyBPMObservers();
+        thread = new Thread(this);
+        stop = false;
+        thread.start();
+    }
+
+    public void off() {
+        stopBeat();
+        stop = true;
+    }
+
+    public void run() {
+        while (!stop) {
+            playBeat();
+            notifyBeatObservers();
+            try {
+                Thread.sleep(60000/getBPM());
+            } catch (Exception e) {}
+        }
+    }
+
+    public void setBPM(int bpm) {
+        this.bpm = bpm;
+        notifyBPMObservers();
+    }
+
+    public int getBPM() {
+        return bpm;
+    }
+
+    // Code to register and notify observers
+    // Audio code to handle the beat
+}
---
+++ ch12/src/views/DJView.java
@@ -0,0 +1,40 @@
+package views;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import models.interfaces.BeatModelInterface;
+
+public class DJView implements ActionListener, BeatObserver, BPMObserver {
+    BeatModelInterface model;
+    ControllerInterface controller;
+    JFrame viewFrame;
+    JPanel viewPanel;
+    BeatBar beatBar;
+    JLabel bpmOutputLabel;
+
+    public DJView(ControllerInterface controller, BeatModelInterface model) {
+        this.controller = controller;
+        this.model = model;
+        model.registerObserver((BeatObserver)this);
+        model.registerObserver((BPMObserver)this);
+    }
+
+    public void createView() {
+        // Create all Swing components here
+    }
+
+    public void updateBPM() {
+        int bpm = model.getBPM();
+        if (bpm == 0) {
+            bpmOutputLabel.setText("offline");
+        } else {
+            bpmOutputLabel.setText("Current BPM: " + model.getBPM());
+        }
+    }
+
+    public void updateBeat() {
+        beatBar.setValue(100);
+    }
+}
