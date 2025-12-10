d4ad75a3d774d319369861551bd840385d27dcea
Graham Strickland <grahamhstrickland@gmail.com>
2025-12-10 12:51:50 +0200

    Move to Maven project

---
+++ src/main/java/com/grahamstrickland/hfdp/App.java
@@ -0,0 +1,10 @@
+package com.grahamstrickland.hfdp;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
